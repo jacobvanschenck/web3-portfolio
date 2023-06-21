6f9ef8e0bfb8e3bc1ca891bd23bf35d6c752e0fa
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2023-06-21 06:59:53 -0500

    adds Main Nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,21 +9,70 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link href="/swap">Swap</Link>
-            </li>
-            <li>
-              <Link href="/buy">Buy</Link>
-            </li>
-          </ul>
-        </nav>
-        {children}
+      <body className="flex">
+        <header>
+          <nav className="flex flex-col justify-between h-full bg-slate-800 px-4 py-6">
+            <div className="flex gap-2 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3"
+                />
+              </svg>
+              <h1 className="text-xl">Web3 Portfolio</h1>
+            </div>
+            <ul className="flex flex-col gap-8">
+              <li>
+                <Link href="/">Dashboard</Link>
+              </li>
+              <li>
+                <Link href="/swap">Swap</Link>
+              </li>
+              <li>
+                <Link href="/buy">Buy</Link>
+              </li>
+            </ul>
+            <div className="flex gap-2 w-full justify-center items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                />
+              </svg>
+            </div>
+          </nav>
+        </header>
+        <main>{children}</main>
       </body>
     </html>
   );
